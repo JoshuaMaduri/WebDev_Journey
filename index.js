f31277b83e5f55f4d783cd19d13b0c176d51cc86
Joshi669 <90431477+Joshi669@users.noreply.github.com>
2024-04-12 17:14:21 -0400

    Added support for icons and working on project card alignment

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class projects {
       new projects(
         "My Blog",
         "This was built with react framework to keep my users up to date on all my discoveries and projects",
-        ["html", "css", "python", "aws"],
+        ["html", "css", "node", "aws", "python"],
         "in-development",
         "github.com",
         "true",
@@ -52,15 +52,27 @@ class projects {
   ]
 
 const logoElements = {
-    "html": '<i class="fa-brands fa-html5"></i>',
-    "css": '<i class="fa-brands fa-css3-alt"></i>',
-    "js": '<i class="fa-brands fa-js"></i>',
-    "react": '<i class="fa-brands fa-react"></i>',
-    "node": '<i class="fa-brands fa-node-js"></i>',
-    "bootstrap": '<i class="fa-brands fa-bootstrap"></i>',
-    "sass": '<i class="fa-brands fa-sass"></i>',
-    "python": '<i class="fa-brands fa-python"></i>',
-    "aws": '<i class="fa-brands fa-aws"></i>',
+    "html": "<img src='static/tech_icons/html-5.svg' id='project-image'></img>",
+    "css": "<img src='static/tech_icons/css-3.svg' id='project-image'></img>",
+    "js": "<img src='static/tech_icons/javascript.svg' id='project-image'></img>", 
+    "react": "<img src='static/tech_icons/react.svg' id='project-image'></img>",
+    "node": "<img src='static/tech_icons/nodejs-icon.svg' id='project-image'></img>",
+    "bootstrap": "<img src='static/tech_icons/bootstrap.svg' id='project-image'></img>",
+    "express": "<img src='static/tech_icons/express.svg' id='project-image'></img>",
+    "typescript": "<img src='static/tech_icons/typescript-icon.svg' id='project-image'></img>",
+    "python": "<img src='static/tech_icons/python.svg' id='project-image'></img>",
+    "flask": "<img src='static/tech_icons/flask.svg' id='project-image'></img>",
+    "django": "<img src='static/tech_icons/django-icon.svg' id='project-image'></img>",
+    "fastapi": "<img src='static/tech_icons/fastapi.svg' id='project-image'></img>",
+    "postgresql": "<img src='static/tech_icons/postgresql.svg' id='project-image'></img>",
+    "aws": "<img src='static/tech_icons/aws.svg' id='project-image'></img>",
+    "azure": "<img src='static/tech_icons/microsoft-azure.svg' id='project-image'></img>",
+    "github": "<img src='static/tech_icons/github-icon.svg' id='project-image'></img>",
+    "docker": "<img src='static/tech_icons/docker-icon.svg' id='project-image'></img>",
+    "kubernetes": "<img src='static/tech_icons/kubernetes.svg' id='project-image'></img>",
+    "elasticsearch": "<img src='static/tech_icons/elasticsearch.svg' id='project-image'></img>",
+    "openshift": "<img src='static/tech_icons/openshift.svg' id='project-image'></img>",
+    "redhat": "<img src='static/tech_icons/redhat.svg' id='project-image'></img>"
 }
 
 app.get("/", (req, res)=> {
