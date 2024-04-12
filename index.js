9f76c1550c9fc63b7565fb15b4141bd4f40ec566
Joshi669 <jmaduri@hotmail.com>
2024-04-11 23:19:16 -0400

    CSS Update main page

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ app.use(bodyParser.urlencoded( { extended: true } )); // Parse form data
 //Project Data
 
 class projects {
-    constructor(name, description, status, githubLink, active, activeLink, screenshotLink) {
+    constructor(name, description, technology, status, githubLink, active, activeLink, screenshotLink) {
       this.name = name;
       this.description = description;
+      this.technology = technology;
       this.status = status;
       this.githubLink = githubLink;
       this.active = active;
@@ -31,19 +32,40 @@ class projects {
       new projects(
       "NHL Stat Center", 
       "This is my first attempt at a full stack application. I use serverless functions to supply hockey information from all organizations to an api that organizes and puts together the data. The easy to navigate user interface is built on plain css and vanilla javascript",
+      ["html", "css", "js", "node", "aws"],
       "in-development",
       "github.com",
       "true",
       "",
-      "")
+      ""),
+      new projects(
+        "My Blog",
+        "This was built with react framework to keep my users up to date on all my discoveries and projects",
+        ["html", "css", "python", "aws"],
+        "in-development",
+        "github.com",
+        "true",
+        "",
+        ""
+      )
   
   ]
 
-
+const logoElements = {
+    "html": '<i class="fa-brands fa-html5"></i>',
+    "css": '<i class="fa-brands fa-css3-alt"></i>',
+    "js": '<i class="fa-brands fa-js"></i>',
+    "react": '<i class="fa-brands fa-react"></i>',
+    "node": '<i class="fa-brands fa-node-js"></i>',
+    "bootstrap": '<i class="fa-brands fa-bootstrap"></i>',
+    "sass": '<i class="fa-brands fa-sass"></i>',
+    "python": '<i class="fa-brands fa-python"></i>',
+    "aws": '<i class="fa-brands fa-aws"></i>',
+}
 
 app.get("/", (req, res)=> {
     try{
-        res.render( "index", {projects_data: project_data} );
+        res.render( "index", {projects_data: project_data, logo_data: logoElements} );
     }catch{
         res.send("404 ERROR");
     }
