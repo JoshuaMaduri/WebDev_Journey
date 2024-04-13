e5eeb7ac507213473c5d39903011ad772732bdd9
Joshi669 <90431477+Joshi669@users.noreply.github.com>
2024-04-13 18:11:34 -0400

    My Projects section completed

--- index.js
+++ index.js
@@ -16,63 +16,60 @@ app.use(bodyParser.urlencoded( { extended: true } )); // Parse form data
 //Project Data
 
 class projects {
-    constructor(name, description, technology, status, githubLink, active, activeLink, screenshotLink) {
+    constructor(name, description, technology, status, githubLink, activeLink, screenshot) {
       this.name = name;
       this.description = description;
       this.technology = technology;
       this.status = status;
       this.githubLink = githubLink;
-      this.active = active;
       this.activeLink = activeLink;
-      this.screenshot = screenshotLink;
+      this.screenshot = screenshot;
     }
   }
   
   const project_data = [
       new projects(
       "NHL Stat Center", 
-      "This is my first attempt at a full stack application. I use serverless functions to supply hockey information from all organizations to an api that organizes and puts together the data. The easy to navigate user interface is built on plain css and vanilla javascript",
+      "This is my first attempt at a full stack application. Using the NHL API and the News API I'm able to pull data directly from official sources withouth having to use a database. The easy to navigate user interface is built on plain css and vanilla javascript",
       ["html", "css", "js", "node", "aws"],
       "in-development",
-      "github.com",
+      "https://github.com/JoshuaMaduri/nhl_stat_center",
       "true",
-      "",
-      ""),
+      "static/nhl-stat-center.jpg"),
       new projects(
         "My Blog",
-        "This was built with react framework to keep my users up to date on all my discoveries and projects",
-        ["html", "css", "node", "aws", "python"],
+        "This was my first project built with react framework. Using features like JWT Based Authorization/Authentication, CRUD system with postgresql, search feature, and an SEO optimization, utilizing AWS for high availability. This project was used to keep my users up to date on all my discoveries and projects, as well as too showcase some of the skills I have picked up. ",
+        ["html", "css", "node", "aws", "js", "node", "react"],
         "in-development",
-        "github.com",
+        "https://github.com/JoshuaMaduri/WebDev_Journey",
         "true",
-        "",
-        ""
+        "static/josh-blog.jpg"
       )
   
   ]
 
 const logoElements = {
-    "html": "<img src='static/tech_icons/html-5.svg' id='project-image'></img>",
-    "css": "<img src='static/tech_icons/css-3.svg' id='project-image'></img>",
-    "js": "<img src='static/tech_icons/javascript.svg' id='project-image'></img>", 
-    "react": "<img src='static/tech_icons/react.svg' id='project-image'></img>",
-    "node": "<img src='static/tech_icons/nodejs-icon.svg' id='project-image'></img>",
-    "bootstrap": "<img src='static/tech_icons/bootstrap.svg' id='project-image'></img>",
-    "express": "<img src='static/tech_icons/express.svg' id='project-image'></img>",
-    "typescript": "<img src='static/tech_icons/typescript-icon.svg' id='project-image'></img>",
-    "python": "<img src='static/tech_icons/python.svg' id='project-image'></img>",
-    "flask": "<img src='static/tech_icons/flask.svg' id='project-image'></img>",
-    "django": "<img src='static/tech_icons/django-icon.svg' id='project-image'></img>",
-    "fastapi": "<img src='static/tech_icons/fastapi.svg' id='project-image'></img>",
-    "postgresql": "<img src='static/tech_icons/postgresql.svg' id='project-image'></img>",
-    "aws": "<img src='static/tech_icons/aws.svg' id='project-image'></img>",
-    "azure": "<img src='static/tech_icons/microsoft-azure.svg' id='project-image'></img>",
-    "github": "<img src='static/tech_icons/github-icon.svg' id='project-image'></img>",
-    "docker": "<img src='static/tech_icons/docker-icon.svg' id='project-image'></img>",
-    "kubernetes": "<img src='static/tech_icons/kubernetes.svg' id='project-image'></img>",
-    "elasticsearch": "<img src='static/tech_icons/elasticsearch.svg' id='project-image'></img>",
-    "openshift": "<img src='static/tech_icons/openshift.svg' id='project-image'></img>",
-    "redhat": "<img src='static/tech_icons/redhat.svg' id='project-image'></img>"
+    "html": "static/tech_icons/html-5.svg",
+    "css": "static/tech_icons/css-3.svg",
+    "js": "static/tech_icons/javascript.svg", 
+    "react": "static/tech_icons/react.svg",
+    "node": "static/tech_icons/nodejs-icon.svg",
+    "bootstrap": "static/tech_icons/bootstrap.svg",
+    "express": "static/tech_icons/express.svg",
+    "typescript": "static/tech_icons/typescript-icon.svg",
+    "python": "static/tech_icons/python.svg",
+    "flask": "static/tech_icons/flask.svg",
+    "django": "static/tech_icons/django-icon.svg",
+    "fastapi": "static/tech_icons/fastapi.svg",
+    "postgresql": "static/tech_icons/postgresql.svg",
+    "aws": "static/tech_icons/aws.svg",
+    "azure": "static/tech_icons/microsoft-azure.svg",
+    "github": "static/tech_icons/github-icon.svg",
+    "docker": "static/tech_icons/docker-icon.svg",
+    "kubernetes": "static/tech_icons/kubernetes.svg",
+    "elasticsearch": "static/tech_icons/elasticsearch.svg",
+    "openshift": "static/tech_icons/openshift.svg",
+    "redhat": "static/tech_icons/redhat.svg"
 }
 
 app.get("/", (req, res)=> {
